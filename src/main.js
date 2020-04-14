2870f5a0d92e0579261590e42c40f64c422202ff
bronskih <dima5202@mail.ru>
2020-04-14 19:37:05 +0500

    Установка VueLidate

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import VueMeta from 'vue-meta'
+import VueLidate from 'vuelidate'
 
 Vue.config.productionTip = false
 Vue.use(VueMeta)
+Vue.use(VueLidate)
 
 new Vue({
   router,
