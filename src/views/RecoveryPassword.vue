aea9bd1caa6f21e0777d5a775dc215415c5cb6da
Nick <54199358+mnick91311@users.noreply.github.com>
2020-04-18 21:44:01 +0300

    view. password recovery

--- src/views/RecoveryPassword.vue
+++ src/views/RecoveryPassword.vue
@@ -1,13 +1,19 @@
 <template>
-
+  <div>
+    <h1>Восстановление пароля</h1>
+    <RecoveryPasswordForm />
+  </div>
 </template>
 
 <script>
+  import RecoveryPasswordForm from '../components/RecoveryPasswordForm'
+
   export default {
     name: 'RecoveryPassword',
     metaInfo: {
       title: 'RecoveryPassword',
     },
+    components: { RecoveryPasswordForm }
   }
 </script>
 
