8f276219effbe869c21d3b420cbf68da4e1c913d
YuliaAnikeeva <yulia-anikeeva@yandex.ru>
2020-04-24 20:41:56 +0300

    regform-confirm password

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -13,13 +13,19 @@
             <div class="error" v-if="!$v.form.password.required">Введите пароль</div>
             <div class="error" v-if="!$v.form.password.betweenLength">{{ $v.form.password.between }}Длина пароля должна быть от {{ $v.form.password.$params.betweenLength.min }} до {{ $v.form.password.$params.betweenLength.max }} символов</div>
         </div>
+         <div class="field" :class="{ 'field--error': $v.form.repeatPassword.$error }">
+            <label class="label" for="passwordRepeat_field">Repeat password</label>
+            <input class="input" id="passwordRepeat_field" type="password" v-model.trim="$v.form.repeatPassword.$model"/>
+            <div class="error" v-if="!$v.form.repeatPassword.sameAsPassword">Passwords must be identical.</div>
+
+         </div>
         <input class="button" type="submit" value="Register" >
     </form>
   </div>
 </template>
 
 <script>
-  import { required, email, minLength, maxLength, and, helpers } from 'vuelidate/lib/validators'
+  import { required, email, minLength, maxLength, and, helpers, sameAs } from 'vuelidate/lib/validators'
 
   const betweenLength = (min, max) => helpers.withParams(
     { min, max },
@@ -27,11 +33,14 @@
   )
 
    export default {
+     name: 'RegistrationForm',
+    props: ['onSuccess'],
     data () {
       return {
         form:{
         email: '',
         password: '',
+        repeatPassword: ''
       }
       }
     },
@@ -45,12 +54,18 @@
       password: {
         required,
         betweenLength: betweenLength(4, 18),
-      }
+      },
+      repeatPassword: {
+      sameAsPassword: sameAs('password')
+    }
       }
     },
     methods: {
      register() {
        console.log(this.form)
+      // this.$store.dispatch('createUser', this.form)
+      // .then(() => this.$router.push('/'))
+      // .catch(err => console.log(err))
      }
     },
 
