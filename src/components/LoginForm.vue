8698218ee194121349ecca86c8e464337144503e
bronskih <dima5202@mail.ru>
2020-05-01 00:03:49 +0500

    update login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,48 +1,57 @@
 <template>
-  <div class="form-login">
-    <div class="message_block">
-    <div v-if="status == 'pending'">Pending...</div>
-    <div class="message message--error" v-else-if="status == 'error'">{{ msg }}</div>
+    <div class="form-login">
+
+        <div class="message_block">
+            <div v-if="status == 'pending'">Pending...</div>
+            <div class="message message--error" v-else-if="status == 'error'">{{ msg }}</div>
+        </div>
+
+        <form class="form" @submit.prevent="onSubmit">
+
+            <div class="group-field" :class="{ 'field--error wobble-error': $v.email.$error}">
+                <input v-model="email" :disabled="disabled" required>
+                <label>Адрес электронной почты</label>
+
+                <div class="error_block">
+                    <div class="error" v-if="($v.email.$dirty && !$v.email.required)">Введите e-mail</div>
+                    <div class="error" v-if="($v.email.$dirty && !$v.email.email)">Введите корректный e-mail</div>
+                </div>
+            </div>
+
+            <div class="group-field" :class="{ 'field--error wobble-error': $v.password.$error }">
+                <input type="password" v-model="password" :disabled="disabled" required>
+                <label>Пароль</label>
+
+                <div class="error_block">
+                    <div class="error" v-if="!$v.password.required">Введите пароль</div>
+                    <div class="error" v-if="!$v.password.betweenLength">{{ $v.password.between }}Длина пароля должна
+                        быть от {{ $v.password.$params.betweenLength.min }} до {{ $v.password.$params.betweenLength.max
+                        }} символов
+                    </div>
+                </div>
+            </div>
+
+            <button class="button" type="submit">Войти</button>
+
+            <router-link class="link" to="/recovery-password">Я не помню пароль</router-link>
+            <router-link class="link" to="/recovery-password">Зарегистрироваться</router-link>
+
+        </form>
+
     </div>
-    <form class="form" @submit.prevent="onSubmit">
-        <div class="field" :class="{ 'field--error': $v.email.$error }">
-            <input class="input" id="email_field" type="email" placeholder="Адрес электронной почты" v-model="$v.email.$model" :disabled="disabled">
-            <!-- <label class="label" for="email_field">Почта</label> -->
-            <div class="hint_container"><p class="hint">Адрес электронной почты</p></div>
-            <div class="error_block">
-            <div class="error" v-if="!$v.email.required">Введите e-mail</div>
-            <div class="error" v-if="!$v.email.email">Введите корректный e-mail</div>
-        </div>
-        </div>
-        <div class="field" :class="{ 'field--error': $v.password.$error }">
-          <div class="hint_container"><p class="hint">Введите пароль</p></div>
-            <!-- <label class="label" for="password_field">Пароль</label> -->
-            <input class="input" id="password_field" type="password" placeholder="Пароль" v-model="$v.password.$model" :disabled="disabled" >
-           <div class="error_block">
-            <div class="error" v-if="!$v.password.required">Введите пароль</div>
-            <div class="error" v-if="!$v.password.betweenLength">{{ $v.password.between }}Длина пароля должна быть от {{ $v.password.$params.betweenLength.min }} до {{ $v.password.$params.betweenLength.max }} символов</div>
-        </div>
-        </div>
-        <input class="button" type="submit" value="Войти" :disabled="$v.$invalid">
-        <a class="link" href="/recovery-password">Я не помню пароль</a>
-        <a class="link" href="/recovery-password">Зарегистрироваться</a>
-        <div>
-          <input type="checkbox" name="agree">
-          <label for="agree" class="agree_check">Даю согласие на обработку моих <a href="#"> персональных данных</a></label>
-        </div>
-        
-    </form>
-  </div>
 </template>
 
 <script>
   import { required, email, minLength, maxLength, and, helpers } from 'vuelidate/lib/validators'
   import { createNamespacedHelpers } from 'vuex'
+
   const { mapGetters } = createNamespacedHelpers('auth')
   import { AUTH_REQUEST } from '../store/auth'
-
   const betweenLength = (min, max) => helpers.withParams(
-    { min, max },
+    {
+      min,
+      max
+    },
     and(minLength(min), maxLength(max))
   )
 
@@ -73,16 +82,22 @@
       }
     },
     methods: {
-      onSubmit: function () {
+      onSubmit () {
         this.$v.$touch()
+        if (this.$v.$invalid) {
+          return
+        }
         if (!this.$v.$invalid) {
           this.messages = []
           const { email, password } = this
-          this.$store.dispatch('auth/'+AUTH_REQUEST, { email, password })
-            .then( (resp) => {
+          this.$store.dispatch('auth/' + AUTH_REQUEST, {
+            email,
+            password
+          })
+            .then((resp) => {
               this.onSuccess && this.onSuccess(resp)
             })
-            .catch( (messages) => {
+            .catch((messages) => {
               this.messages = messages
             })
         }
@@ -92,169 +107,166 @@
 </script>
 
 <style lang="scss" scoped>
-.message_block{
-  height: 30px;
-}
- .form {
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-
-    @media (min-width: 425px) {
-      width: 400px;
+    .message_block {
+        height: 30px;
     }
-  }
-  .field {
-    display: flex;
-    flex-direction: column;
-    
-  }
-  // .label {
-  //   margin: 20px 79px;
-  //   align-self: center;
-  //   // text-transform: uppercase;
-  //   font-size: 0.8em;
-  //   margin-bottom: 5px;
 
-  //   /* Введите адрес электронной почты */
-  //   width: 293px;
-  //   height: 20px;
+    .form {
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
 
-  //   font-family: Montserrat;
-  //   font-style: normal;
-  //   font-weight: normal;
-  //   font-size: 16px;
-  //   line-height: 20px;
-
-  //   /* Text color test */
-  //   color: #1A1A22;
-
-  // }
-  .input {
-    margin: 20px 0 0 0;
-    // border-radius: 5px;
-    // border: 1px solid #333;
-
-    /* Rectangle 7 */
-    width: 360px;
-    height: 22px;
-    background: #FAFAFA;
-    border-radius: 10px;
-    /* ZooBro@test.ru */
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 22px;
-    padding: 30px 20px 8px 20px;
-    letter-spacing: 0.2px;
-    color: #1A1A22;
-  }
-
-  .input:enabled {
-    background: #FFFFFF;
-  }
-  .hint_container{
-    position: relative;
-  }
-  .hint{
-    display: none;
-    position: absolute;
-    left: 20px;
-    top: 20px;
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 15px;
-    color: #B5B6B6;
-  }
-  .input:enabled +.hint{
-    display: block;
-    left: 460px;
-    top: 299px;
+        @media (min-width: 425px) {
+            width: 400px;
+        }
     }
-  .link {
-    margin: 10px 0;
-    width: 155px;
-    height: 20px;
-    text-decoration: none;
-    border-bottom: 0.3px solid #1A1A22;
-    color:  #1A1A22;
-    align-self: center;
-  }
-  
-  .button {
-    margin: 20px 0;
-    padding: 15px;
-    align-self: center;
-    /* Rectangle 8 */
-    width: 255px;
-    height: 55px;
-    background: #4D99BA;
-    background-image:  url(../assets/paw.svg);
-    background-position: 80% 50%;
-    background-repeat: no-repeat;
-    opacity: 0.8;
-    border-radius: 10px;
-    /* Далее */
 
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 22px;
-    line-height: 27px;
-    /* identical to box height */
-    
-    letter-spacing: 0.2px;
-
-    /* Gray 6 */
-    color: #F2F2F2;
-
-  }
-  
-  .agree_check {
-    /* Даю согласие на обработку моих персональных данных */
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 15px;
-    color: #1A1A22;
-
-  }
-  .agree_check a{
-    color:#1A1A22;
-    text-decoration: none;
-    border-bottom: 0.3px solid #1A1A22;
-  }
-   .error {
-    margin-top: 5px;
-    align-self: flex-start;
-    display: none;
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 11px;
-    line-height: 13px;
-    color: #F63535;
-  }
-  .field--error .input {
-    border-color: #F63535;
-  }
-  .field--error .error {
-    display: block;
-  }
-  .error_block{
-    height: 20px;
-  }
-  .message {
-    &--success {
-      color: green;
+    .link {
+        margin: 10px 0;
+        width: 155px;
+        height: 20px;
+        text-decoration: none;
+        border-bottom: 0.3px solid #1A1A22;
+        color: #1A1A22;
+        align-self: center;
     }
-    &--error {
-      color:  #F63535;
+
+    .button {
+        margin: 20px 0;
+        padding: 15px 90px;
+        align-self: center;
+        background: #2289B5;
+        background-image: url(../assets/paw.svg);
+        background-position: calc(100% - 30px) 50%;
+        background-repeat: no-repeat;
+        opacity: 0.8;
+        border-radius: 10px;
+        font-family: Montserrat, serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 22px;
+        line-height: 27px;
+        color: #F2F2F2;
+        border: none;
     }
-  }
-  
+
+    .button:hover {
+        background-color: #4D99BA;
+    }
+
+    button:focus,
+    button:active {
+        outline-color: transparent;
+    }
+
+    .error {
+        margin-top: 5px;
+        align-self: flex-start;
+        display: none;
+        font-family: Montserrat, serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 11px;
+        line-height: 13px;
+        color: #F63535;
+    }
+
+    .field--error input {
+        border-color: #F63535 !important;
+    }
+
+    .field--error .error {
+        display: block;
+    }
+
+    .error_block {
+        height: 20px;
+    }
+
+    .message {
+        &--success {
+            color: green;
+        }
+
+        &--error {
+            color: #F63535;
+        }
+    }
+
+
+    .group-field {
+        position: relative;
+    }
+
+    .group-field input {
+        font-size: 18px;
+        padding: 30px 20px 10px 20px;
+        display: block;
+        box-sizing: border-box;
+        width: 100%;
+        border: 2px solid #FAFAFA;
+        background: #FAFAFA;
+        border-radius: 10px;
+        color: #1A1A22;
+        transition: 0.2s ease all;
+    }
+
+    .group-field input:focus {
+        outline: none;
+    }
+
+    .group-field label {
+        color: #B5B6B6;
+        font-size: 18px;
+        font-weight: normal;
+        position: absolute;
+        pointer-events: none;
+        left: 20px;
+        top: 23px;
+        transition: 0.2s ease all;
+        -moz-transition: 0.2s ease all;
+        -webkit-transition: 0.2s ease all;
+    }
+
+    .group-field input:focus ~ label,
+    .group-field input:valid ~ label {
+        top: 10px;
+        font-size: 14px;
+    }
+
+    .group-field input:focus,
+    .group-field input:hover,
+    .group-field input:valid {
+        background: #FFFFFF;
+        border-color: #2289B5;
+    }
+
+
+    .wobble-error {
+        animation: wobble-error 0.8s both;
+    }
+
+    @keyframes wobble-error {
+        0%,
+        100% {
+            transform: translateX(0%);
+            transform-origin: 50% 50%;
+        }
+        15% {
+            transform: translateX(-15px);
+        }
+        30% {
+            transform: translateX(7.7px);
+        }
+        45% {
+            transform: translateX(-7.5px);
+        }
+        60% {
+            transform: translateX(4.5px);
+        }
+        75% {
+            transform: translateX(-3px);
+        }
+    }
+
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container">
-  <div class="border">
-    <h1>Вход</h1>
-    <LoginForm :onSuccess="onSuccess"/>
-  </div>
-  </div>
+    <div class="container">
+        <div class="border">
+            <h1>Вход</h1>
+            <LoginForm :onSuccess="onSuccess"/>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -25,35 +25,31 @@
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat');
-.container {
-  background: #F6FBFE;
-  padding: 85px 0;
-}
-.border{
-/* Rectangle 98 */
-display: flex;
-flex-direction: column;
-width: 450px;
-height: 550px;
-background: #FFFFFF;
-border-radius: 20px;
-margin: 0px auto;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat');
 
-h1{
-/* width: 223px;
-height: 39px; */
-margin: 45px 114px 0 113px;
-justify-content: center;
-font-family: Montserrat;
-font-style: normal;
-font-weight: 600;
-font-size: 32px;
-line-height: 39px;
-/* identical to box height */
-letter-spacing: 0.1px;
-color: #1A1A22;
+    .container {
+        background: #F6FBFE;
+        padding: 85px 0;
+    }
 
-}
+    .border {
+        display: flex;
+        flex-direction: column;
+        width: 450px;
+        padding: 50px 0;
+        background: #FFFFFF;
+        border-radius: 20px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        justify-content: center;
+        font-family: Montserrat, serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 32px;
+        line-height: 39px;
+        letter-spacing: 0.1px;
+        color: #1A1A22;
+    }
 </style>
