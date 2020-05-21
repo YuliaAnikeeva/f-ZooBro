bae2153f3f3e21749dff3cc13f3a77a7a3dca943
bronskih <dima5202@mail.ru>
2020-05-21 20:03:36 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  pwa: {
+    name: 'Коробочка счастья ZooBro',
+    themeColor: '#2289B5',
+    msTileColor: '#2289B5',
+    manifestOptions: {
+      background_color: '#2289B5'
+    }
+  }
 }
