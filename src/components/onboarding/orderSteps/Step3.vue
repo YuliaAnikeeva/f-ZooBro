0f65464907018603ef2d1e1c878777eafcb549c6
bronskih <dima5202@mail.ru>
2020-05-24 21:35:54 +0500

    Update Step3.vue

--- src/components/onboarding/orderSteps/Step3.vue
+++ src/components/onboarding/orderSteps/Step3.vue
@@ -1,124 +1,137 @@
 <template>
-    <form @submit.prevent="submitHandler" class="form_contact form_small">
-        <div class="form-section">
+        <form @submit.prevent="submitHandler" class="form_contact form_small">
+            {{showMap}}
+            <div class="form-section">
 
-            <div :class="{ 'field--error wobble-error': $v.user_name.$error }" class="form-group">
-                <label class="form-group__label">Ваше имя</label>
-                <div class="form-group__content">
-                    <div class="input">
-                        <input
-                                type="text"
-                                placeholder="Петя"
-                                v-model="user_name"
-                                class="input__control"
-                        />
+                <div :class="{ 'field--error wobble-error': $v.user_name.$error }" class="form-group">
+                    <label class="form-group__label">Ваше имя</label>
+                    <div class="form-group__content">
+                        <div class="input">
+                            <input
+                                    type="text"
+                                    placeholder="Петя"
+                                    v-model="user_name"
+                                    class="input__control"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div class="form-group__helper">
-                    <div class="form-group__errors">
-                        <div class="error" v-if="$v.user_name.$dirty && !$v.user_name.required">Введите ваше имя</div>
-                    </div>
-                </div>
-            </div>
-
-            <div :class="{ 'field--error wobble-error': $v.email.$error }"  class="form-group">
-                <label class="form-group__label">E-mail</label>
-                <div class="form-group__content">
-                    <div class="input">
-                        <input
-                                type="text"
-                                placeholder="user@example.com"
-                                v-model="email"
-                                class="input__control"
-                        />
-                    </div>
-                </div>
-                <div class="form-group__helper">
-                    <div class="form-group__errors">
-                        <div class="error" v-if="$v.email.$dirty && !$v.email.required">Введите email</div>
-                        <div class="error" v-if="$v.email.$dirty && !$v.email.email">Введите корректный email
+                    <div class="form-group__helper">
+                        <div class="form-group__errors">
+                            <div class="error" v-if="$v.user_name.$dirty && !$v.user_name.required">Введите ваше имя</div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div :class="{ 'field--error wobble-error': $v.phone.$error }" class="form-group">
-                <label class="form-group__label">Телефон</label>
-                <div class="form-group__content">
-                    <div class="input">
-                        <the-mask
-                                type="tel"
-                                placeholder="+7 (000) 000-00-00"
-                                v-model.trim.number="phone"
-                                mask="+7 (###) ###-##-##"
-                                class="input__control"
-                        />
+                <div :class="{ 'field--error wobble-error': $v.email.$error }"  class="form-group">
+                    <label class="form-group__label">E-mail</label>
+                    <div class="form-group__content">
+                        <div class="input">
+                            <input
+                                    type="text"
+                                    placeholder="user@example.com"
+                                    v-model="email"
+                                    class="input__control"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div class="form-group__helper">
-                    <div class="form-group__errors">
-                        <div class="error" v-if="$v.phone.$dirty && !$v.phone.required">Введите телефон</div>
-                        <div class="error" v-if="$v.phone.$dirty && !$v.phone.minLength">Введите корректный
-                            номер телефона
+                    <div class="form-group__helper">
+                        <div class="form-group__errors">
+                            <div class="error" v-if="$v.email.$dirty && !$v.email.required">Введите email</div>
+                            <div class="error" v-if="$v.email.$dirty && !$v.email.email">Введите корректный email
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
-        <div class="form-section">
-
-            <div :class="{ 'field--error wobble-error': $v.address.$error }"  class="form-group form-group_with_link">
-                <label class="form-group__label">Адрес доставки</label>
-                <div class="form-group__content">
-                    <div class="input input_type_address">
-                        <input
-                                type="text"
-                                placeholder="г. Москва ул. Ленина 35 кв.10"
-                                v-model="address"
-                                class="input__control"
-                        />
-                    </div>
-                </div>
-                <div class="form-group__helper">
-                    <div class="form-group__errors">
-                        <div class="error" v-if="$v.address.$dirty && !$v.address.required">Введите адрес</div>
-                    </div>
-                    <a class="form-group__link" href="#" @click="showMap = !showMap">Зоны доставки</a>
-                </div>
-            </div>
-
-            <div :class="{ 'field--error wobble-error': ($v.date_delivery.$error || $v.time_delivery.$error) }"  class="form-group">
-                <label class="form-group__label">Желаемые дата и время доставки</label>
-                <div class="form-group__content">
-                    <div class="input-group">
-                        <div class="input input_type_date">
-                            <input type="date" class="input__control input__control_type_datetime"
-                                    v-model="date_delivery">
+                <div :class="{ 'field--error wobble-error': $v.phone.$error }" class="form-group">
+                    <label class="form-group__label">Телефон</label>
+                    <div class="form-group__content">
+                        <div class="input">
+                            <the-mask
+                                    type="tel"
+                                    placeholder="+7 (000) 000-00-00"
+                                    v-model.trim.number="phone"
+                                    mask="+7 (###) ###-##-##"
+                                    class="input__control"
+                            />
                         </div>
-                        <div class="input input_type_time">
-                            <input type="time" class="input__control input__control_type_datetime"
-                                    v-model="time_delivery">
+                    </div>
+                    <div class="form-group__helper">
+                        <div class="form-group__errors">
+                            <div class="error" v-if="$v.phone.$dirty && !$v.phone.required">Введите телефон</div>
+                            <div class="error" v-if="$v.phone.$dirty && !$v.phone.minLength">Введите корректный
+                                номер телефона
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="form-group__helper">
-                    <div class="form-group__errors">
-                        <div class="error" v-if="$v.date_delivery.$dirty && !$v.date_delivery.required">Введите желаемую дату доставки</div>
-                        <div class="error" v-if="$v.time_delivery.$dirty && !$v.time_delivery.required">Введите желаемое время доставки</div>
+
+            </div>
+            <div class="form-section">
+
+                <div :class="{ 'field--error wobble-error': $v.address.$error }"  class="form-group form-group_with_link">
+                    <label class="form-group__label">Адрес доставки</label>
+                    <div class="form-group__content">
+                        <div class="input input_type_address">
+                            <input
+                                    type="text"
+                                    placeholder="г. Москва ул. Ленина 35 кв.10"
+                                    v-model="address"
+                                    class="input__control"
+                            />
+                        </div>
+                    </div>
+                    <div class="form-group__helper">
+                        <div class="form-group__errors">
+                            <div class="error" v-if="$v.address.$dirty && !$v.address.required">Введите адрес</div>
+                        </div>
+                        <a class="form-group__link" href="#" @click="showMap = true">Зоны доставки</a>
                     </div>
                 </div>
+
+                <div :class="{ 'field--error wobble-error': ($v.date_delivery.$error || $v.time_delivery.$error) }"  class="form-group">
+                    <label class="form-group__label">Желаемые дата и время доставки</label>
+                    <div class="form-group__content">
+                        <div class="input-group">
+                            <div class="input input_type_date">
+                                <input type="date" class="input__control input__control_type_datetime"
+                                       v-model="date_delivery">
+                            </div>
+                            <div class="input input_type_time">
+                                <input type="time" class="input__control input__control_type_datetime"
+                                       v-model="time_delivery">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group__helper">
+                        <div class="form-group__errors">
+                            <div class="error" v-if="$v.date_delivery.$dirty && !$v.date_delivery.required">Введите желаемую дату доставки</div>
+                            <div class="error" v-if="$v.time_delivery.$dirty && !$v.time_delivery.required">Введите желаемое время доставки</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
-        </div>
-    </form>
+            <vue-modaltor
+                    :visible="showMap"
+                    :resize-width='{1920:"940px",940:"90%"}'
+                    :bg-overlay="' rgba(41, 41, 41, 0.4)'"
+                    :bg-panel="'#fff'"
+                    @hide="showMap = false">
+                <YandexMap :updateAddress="updateAddress"/>
+            </vue-modaltor>
+
+        </form>
 </template>
 
 <script>
   import { email, required, minLength, maxLength, numeric } from 'vuelidate/lib/validators'
+  import YandexMap from '../../YandexMap'
 
   export default {
     name: 'Step3',
+    components: { YandexMap },
     props: ['order'],
     validations () {
       return {
@@ -147,11 +160,12 @@
     },
     data: () => ({
       showMap: false,
+      address: null,
     }),
     computed: {
       ...(() => {
         let o = {}
-        let f = ["user_name", "email", "phone", "address", "date_delivery", "time_delivery"]
+        let f = ["user_name", "email", "phone", "date_delivery", "time_delivery"]
         f.forEach( n => o[[n]] = {
           get () {
             return this.order[n]
@@ -162,7 +176,14 @@
         })
         return o
       })(),
-    }
+    },
+    methods:{
+      updateAddress(address){
+        console.log('address', address)
+        this.address = address
+        this.showMap = false
+      },
+    },
   }
 </script>
 
